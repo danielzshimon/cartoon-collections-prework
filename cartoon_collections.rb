8867491c4b73d00b7cc7ea186133610bc63d516a
Daniel Shimon <danielzshimon@gmail.com>
2018-04-13 01:45:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect do |element|
-  element.upcase + "!"
+  array.collect { |call| call.capitalize + "!" }
 end
 
 def long_planeteer_calls(array)
